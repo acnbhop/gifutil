b1125ed1386292b00b2db46f1ff0e76d9681f180
grant <abernathy.grant@icloud.com>
2025-12-31 22:26:10 -0600

    inspect init

---
+++ inspect.c
@@ -0,0 +1,11 @@
+/*
+ *  (C) Copyright 2025/2026 Grant Abernathy & Kendalynn Kohler. All Rights Reserved.
+ *
+ *      inspect.c: Inspect Source File.
+ */
+
+/* core header file */
+#include "core.h"
+
+/* file header */
+#include "inspect.h"
---
+++ inspect.h
@@ -0,0 +1,9 @@
+/*
+ *  (C) Copyright 2025/2026 Grant Abernathy & Kendalynn Kohler. All Rights Reserved.
+ *
+ *      inspect.h: Inspect Header File.
+ */
+#ifndef INSPECT_H
+#define INSPECT_H
+
+#endif /* INSPECT_H */
