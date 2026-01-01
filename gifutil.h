16028903ecbd9c82403219b9628db65b6dff3d09
grant <abernathy.grant@icloud.com>
2025-12-31 22:38:18 -0600

    raw_data is u8 and use typedefs

--- gifutil.h
+++ gifutil.h
@@ -1,7 +1,7 @@
 /*
- *  (C) Copyright 2025/2026 Grant Abernathy & Kendalynn Kohler. All Rights Reserved.
+ * (C) Copyright 2025/2026 Grant Abernathy & Kendalynn Kohler. All Rights Reserved.
  *
- *      gifutil.h: Core Header File For gifutil.
+ * gifutil.h: Core Header File For gifutil.
  */
 #ifndef GIFUTIL_H
 #define GIFUTIL_H
@@ -23,16 +23,17 @@ typedef enum gif_error
 /* context holding loaded gif data */
 typedef struct gif_context
 {
-    unsigned char* raw_data; /* pointer to raw image buffer, which contains all the frames. */
-    int width;          /* width of each frame */
-    int height;         /* height of each frame */
-    int frames;         /* total number of frames, z-depth */
-    int channels;       /* components per pixel, 4 for rgba usually */
-    int* delays;        /* array of delays in ms for each frames*/
+    u8* raw_data;       /* pointer to raw image buffer (all frames) */
+    s32 width;          /* width of each frame */
+    s32 height;         /* height of each frame */
+    s32 frames;         /* total number of frames (z-depth) */
+    s32 channels;       /* components per pixel (4 for rgba usually) */
+    s32* delays;        /* array of delays in ms for each frame */
 } gif_context;
 
 /* loads a gif from disk into memory. */
 gif_error gif_load(const char* filepath, gif_context* out_context);
+
 /* frees resources that were allocated. */
 void gif_free(gif_context* context);
 
